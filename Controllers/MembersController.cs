d51d50d819e4f75614443e38c31498cae50eb1e6
Mohamed Abdallah <mohamedabdallah1542001@gmail.com>
2026-01-12 21:17:01 +0200

    initalCommit

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController (DatingContext context): ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<IReadOnlyList<AppUser>> GetAllMembers()
+        {
+            var users = context.Users.ToList();
+
+            return Ok(users);
+        }
+
+
+        [HttpGet("{Id}")]
+
+        public ActionResult<AppUser> GetMemberById(string Id)
+        {
+
+            var user = context.Users.Find(Id);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+
+
+    }
+}
